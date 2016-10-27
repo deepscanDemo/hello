7d997d1d703334e89cdd34cd2f2be7d58d358d3e
deepscanDemo <deepscanDemo@gmail.com>
2016-10-27 17:31:25 +0900

    Update bugs.js

--- bugs.js
+++ bugs.js
@@ -9,4 +9,19 @@ function loadingClose(obj) {
 
 
 
+var obj = {
+ name: 'name',
+  age: 15,
+  size: 105,
+  name: 'noname'
+}
+
+
+var value = function (a, b) {
+  nextFunc(a, b);
+}
+
+value.prototype.nextFunc = function () {
+  return 'next'; 
+}
 
